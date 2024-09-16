7916fa1fbaaa8a6757b2ccc71f988be13f8cd203
CiroAlfa <cimonrincon01@gmail.com>
2024-09-15 19:18:00 -0500

    add serializer

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,8 +1,18 @@
 from django.contrib import admin 
-from django.urls import path 
+from django.urls import path, include
+from rest_framework import routers
+
 #from .views import sayHello 
 from . import views
-urlpatterns = [ 
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+
+urlpatterns = [
     #path('', sayHello, name='sayHello'),
-    path('', views.index, name='index' )
-]
+    path('', include(router.urls)),
+    path('', views.index, name='index' ),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+
+    
+    ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -13,3 +13,13 @@ from django.shortcuts import render'''
 def index(request):
     return render(request, 'index.html', {})
 
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .serializers import UserSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
